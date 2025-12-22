66b63b9b595a35de6682488511761c596a75e2a0
MatFragg <u202318323@upc.edu.pe>
2025-12-21 21:16:57 -0500

    feat(products): implement product repositoryr

--- src/main/java/com/matfragg/shopping_car/api/products/repository/ProductRepository.java
+++ src/main/java/com/matfragg/shopping_car/api/products/repository/ProductRepository.java
@@ -1,4 +1,13 @@
 package com.matfragg.shopping_car.api.products.repository;
 
-public class ProductRepository {
+import com.matfragg.shopping_car.api.products.model.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findProductsBySellerId(Long sellerId);
+    List<Product> findByActiveAndAvailableAndSellerIdNot(boolean active, boolean available, Long sellerId);
 }
