98dda775cde5f83574fc9d436556a4a25267c9eb
MatFragg <u202318323@upc.edu.pe>
2025-12-21 21:16:21 -0500

    feat(products): add response for a product

--- src/main/java/com/matfragg/shopping_car/api/products/dto/response/ProductResponse.java
+++ src/main/java/com/matfragg/shopping_car/api/products/dto/response/ProductResponse.java
@@ -1,4 +1,18 @@
 package com.matfragg.shopping_car.api.products.dto.response;
 
-public record ProductResponse() {
-}
+import java.math.BigDecimal;
+import java.util.Date;
+
+public record ProductResponse(
+        Long id,
+        String name,
+        String description,
+        Double price,
+        Integer stock,
+        String category,
+        String imageUrl,
+        Long sellerId,
+        Boolean active,
+        Boolean available,
+        Date createdAt
+) {}
