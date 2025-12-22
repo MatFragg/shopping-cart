acebb79e6729745b913070a029c6d45796e6533b
MatFragg <u202318323@upc.edu.pe>
2025-12-22 00:48:20 -0500

    feat(shoppingcart): add response dtos for shoppingcart module.

--- src/main/java/com/matfragg/shopping_car/api/shoppingcart/dto/response/CartItemResponse.java
+++ src/main/java/com/matfragg/shopping_car/api/shoppingcart/dto/response/CartItemResponse.java
@@ -1,4 +1,13 @@
 package com.matfragg.shopping_car.api.shoppingcart.dto.response;
 
-public record CartItemResponse() {
-}
+import java.math.BigDecimal;
+
+public record CartItemResponse(
+        Long id,
+        Long productId,
+        Long sellerId,
+        String productName,
+        Integer quantity,
+        BigDecimal unitPrice,
+        BigDecimal subtotal
+) {}
--- src/main/java/com/matfragg/shopping_car/api/shoppingcart/dto/response/CartResponse.java
+++ src/main/java/com/matfragg/shopping_car/api/shoppingcart/dto/response/CartResponse.java
@@ -1,4 +1,16 @@
 package com.matfragg.shopping_car.api.shoppingcart.dto.response;
 
-public interface CartResponse {
-}
+import com.matfragg.shopping_car.api.shoppingcart.model.enums.CartStatus;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public record CartResponse(
+        Long id,
+        Long customerId,
+        CartStatus status,
+        BigDecimal total,
+        List<CartItemResponse> items,
+        Date createdAt,
+        Date updatedAt
+) {}
