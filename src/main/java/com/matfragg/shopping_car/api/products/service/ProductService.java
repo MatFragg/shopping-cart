54ba32361601adbae700b83b8eaecf1792e94885
MatFragg <u202318323@upc.edu.pe>
2025-12-21 23:05:04 -0500

    feat(products): add new methods to product service.

--- src/main/java/com/matfragg/shopping_car/api/products/service/ProductService.java
+++ src/main/java/com/matfragg/shopping_car/api/products/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.matfragg.shopping_car.api.products.service;
 
 import com.matfragg.shopping_car.api.products.dto.request.CreateProductRequest;
+import com.matfragg.shopping_car.api.products.dto.request.UpdateProductRequest;
 import com.matfragg.shopping_car.api.products.dto.response.ProductResponse;
 
 import java.util.List;
@@ -8,9 +9,11 @@ import java.util.List;
 public interface ProductService {
 
     ProductResponse createProduct(Long sellerId, CreateProductRequest request);
-
-    List<ProductResponse> findBySellerId(Long sellerId);
-
-    List<ProductResponse> findAvailableProducts(Long excludeSellerId);
+    ProductResponse updateProduct(Long productId, UpdateProductRequest request);
+    ProductResponse deleteProduct(Long productId);
+    ProductResponse getProductById(Long productId);
+    List<ProductResponse> getAllProducts();
+    List<ProductResponse> getProductsBySellerId(Long sellerId);
+    List<ProductResponse> getAvailableProducts(Long excludeSellerId);
 
 }
--- src/main/java/com/matfragg/shopping_car/api/products/service/impl/ProductServiceImpl.java
+++ src/main/java/com/matfragg/shopping_car/api/products/service/impl/ProductServiceImpl.java
@@ -1,8 +1,10 @@
 package com.matfragg.shopping_car.api.products.service.impl;
 
-import com.matfragg.shopping_car.api.customers.dto.response.CustomerResponse;
+import com.matfragg.shopping_car.api.customers.model.entities.Customer;
+import com.matfragg.shopping_car.api.customers.repository.CustomerRepository;
 import com.matfragg.shopping_car.api.customers.service.CustomerService;
 import com.matfragg.shopping_car.api.products.dto.request.CreateProductRequest;
+import com.matfragg.shopping_car.api.products.dto.request.UpdateProductRequest;
 import com.matfragg.shopping_car.api.products.dto.response.ProductResponse;
 import com.matfragg.shopping_car.api.products.mapper.ProductMapper;
 import com.matfragg.shopping_car.api.products.model.entities.Product;
@@ -13,33 +15,40 @@ import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final CustomerRepository customerRepository;
     private final CustomerService customerService;
     private final ProductMapper productMapper;
 
     public ProductServiceImpl(
             ProductRepository productRepository,
+            CustomerRepository customerRepository,
             CustomerService customerService,
             ProductMapper productMapper
     ) {
         this.productRepository = productRepository;
+        this.customerRepository = customerRepository;
         this.customerService = customerService;
         this.productMapper = productMapper;
     }
 
     @Override
     public ProductResponse createProduct(Long sellerId, CreateProductRequest request) {
-        CustomerResponse seller = customerService.findById(sellerId);
+        Customer seller = customerRepository.findById(sellerId).orElseThrow(() -> new BusinessException("Seller not found"));;
 
-        if (request.price().compareTo((double) 0) <= 0)
+        if (seller == null)
+            throw new BusinessException("Seller not found with id: " + sellerId);
+
+        if (request.price() == null || request.price() <= 0.0)
             throw new BusinessException("Price must be greater than zero");
 
-        if (request.stock() < 0)
+        if (request.stock() == null || request.stock() < 0)
             throw new BusinessException("Stock cannot be negative");
 
         Product product = new Product(
@@ -49,7 +58,7 @@ public class ProductServiceImpl implements ProductService {
                 request.stock(),
                 request.category(),
                 request.imageUrl(),
-                sellerId,
+                seller,
                 true,
                 request.stock() > 0
         );
@@ -60,13 +69,52 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public List<ProductResponse> findBySellerId(Long sellerId) {
+    public ProductResponse updateProduct(Long productId, UpdateProductRequest request) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BusinessException("Product not found with id: " + productId));
+
+        product.updateProduct(request.name(),
+                request.description(),
+                request.price(),
+                request.stock(),
+                request.category(),
+                request.imageUrl(),
+                request.active(),
+                request.stock() > 0
+        );
+        Product updated = productRepository.save(product);
+        return productMapper.toResponse(updated);
+    }
+
+    @Override
+    public ProductResponse deleteProduct(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BusinessException("Product not found with id: " + productId));
+        productRepository.delete(product);
+        return productMapper.toResponse(product);
+    }
+
+    @Override
+    public ProductResponse getProductById(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BusinessException("Product not found with id: " + productId));
+        return productMapper.toResponse(product);
+    }
+
+    @Override
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return productMapper.toResponseList(products);
+    }
+
+    @Override
+    public List<ProductResponse> getProductsBySellerId(Long sellerId) {
         List<Product> products = productRepository.findProductsBySellerId(sellerId);
         return productMapper.toResponseList(products);
     }
 
     @Override
-    public List<ProductResponse> findAvailableProducts(Long excludeSellerId) {
+    public List<ProductResponse> getAvailableProducts(Long excludeSellerId) {
         List<Product> products = productRepository.findByActiveAndAvailableAndSellerIdNot(true, true, excludeSellerId);
         return productMapper.toResponseList(products);
     }
