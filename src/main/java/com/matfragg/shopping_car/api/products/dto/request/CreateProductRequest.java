4999cf957780a1104f46a63ab00e1654c2ca3d63
MatFragg <u202318323@upc.edu.pe>
2025-12-21 21:15:48 -0500

    feat(products): add request to create a product

--- src/main/java/com/matfragg/shopping_car/api/products/dto/request/CreateProductRequest.java
+++ src/main/java/com/matfragg/shopping_car/api/products/dto/request/CreateProductRequest.java
@@ -1,4 +1,6 @@
 package com.matfragg.shopping_car.api.products.dto.request;
 
-public record CreateProductRequest() {
+import java.math.BigDecimal;
+
+public record CreateProductRequest(String name, String description, Double price, Integer stock, String category, String imageUrl, boolean active, Boolean available, String createdAt) {
 }
