e131ba8414c02574a6bb6bde1641ec98d948f81e
MatFragg <u202318323@upc.edu.pe>
2025-12-21 23:03:06 -0500

    refact(products): solve problem in create request for product

--- src/main/java/com/matfragg/shopping_car/api/products/dto/request/CreateProductRequest.java
+++ src/main/java/com/matfragg/shopping_car/api/products/dto/request/CreateProductRequest.java
@@ -1,6 +1,5 @@
 package com.matfragg.shopping_car.api.products.dto.request;
 
-import java.math.BigDecimal;
 
-public record CreateProductRequest(String name, String description, Double price, Integer stock, String category, String imageUrl, boolean active, Boolean available, String createdAt) {
+public record CreateProductRequest(String name, String description, Double price, Integer stock, String category, String imageUrl, boolean active, Boolean available) {
 }
