e0d54b900385eb4c321858bf17a0bd33d778fc9f
MatFragg <u202318323@upc.edu.pe>
2025-12-21 21:21:35 -0500

    feat(customers): add new methods to customer service

--- src/main/java/com/matfragg/shopping_car/api/customers/service/CustomerService.java
+++ src/main/java/com/matfragg/shopping_car/api/customers/service/CustomerService.java
@@ -4,8 +4,12 @@ import com.matfragg.shopping_car.api.customers.dto.request.CreateCustomerRequest
 import com.matfragg.shopping_car.api.customers.dto.request.UpdateCustomerRequest;
 import com.matfragg.shopping_car.api.customers.dto.response.CustomerResponse;
 
+import java.util.List;
+
 public interface CustomerService {
     CustomerResponse createCustomer(CreateCustomerRequest request);
     CustomerResponse findByUserId(Long userId);
+    CustomerResponse findCustomerById(Long customerId);
+    List<CustomerResponse> findAllCustomers();
     CustomerResponse updateCustomer(Long userId, UpdateCustomerRequest request);
 }
