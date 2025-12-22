e512130d211b5eef9e1bd832b88e66f5757b4684
MatFragg <u202318323@upc.edu.pe>
2025-12-21 21:16:02 -0500

    feat(products): add request to update a product

--- src/main/java/com/matfragg/shopping_car/api/products/dto/request/UpdateProductRequest.java
+++ src/main/java/com/matfragg/shopping_car/api/products/dto/request/UpdateProductRequest.java
@@ -1,4 +1,4 @@
 package com.matfragg.shopping_car.api.products.dto.request;
 
-public record UpdateProductRequest() {
+public record UpdateProductRequest(String name, String description, Double price, Integer stock, String category, String imageUrl, Boolean active, Boolean available) {
 }
